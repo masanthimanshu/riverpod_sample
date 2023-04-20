937e49329634cb638577ab53050eb470ca85b009
Himanshu Masant <himanshumasant@uxdlabtech.com>
2023-04-20 16:36:51 +0530

    Updates

--- lib/network/requests.dart
+++ lib/network/requests.dart
@@ -5,7 +5,7 @@ class GetRequest {
     final uri = Uri.parse(url);
     final res = await http.get(uri);
 
-    if (res.statusCode == 200) {
+    if (res.statusCode >= 200 && res.statusCode <= 208) {
       final json = res.body;
       return json;
     }
